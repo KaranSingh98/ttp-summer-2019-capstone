26a986a8b558fdc7cf892276b67ca6e82cd29c22
Karanveer Singh <karansingh98123@gmail.com>
2019-06-14 13:58:15 -0400

    starting work on the home page. Added the name of the app

---
+++ capstone-2019/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <header>
+                    <h1> Basketcase </h1>
+                </header>
+            </div>
+
+        );
+
+    }
+};
+
+export default Home;
