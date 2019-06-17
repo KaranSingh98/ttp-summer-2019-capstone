2478f412d91759e375efb0adf6455b08416420e3
RatulAhmed <ratulahmed1@gmail.com>
2019-06-17 11:58:37 -0400

    Able to search for players now

---
+++ capstone-2019/src/components/PlayerResults.jsx
@@ -0,0 +1,19 @@
+/* PlayerResults Smart Component that will 
+ *  handle the results from the api/database 
+ *  call based on the search results 
+ * Should retrieve search results from Store
+*/ 
+
+import React, {Component} from 'react';
+
+class PlayerResults extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state {
+			results: []
+		}
+
+
+	}
+}
