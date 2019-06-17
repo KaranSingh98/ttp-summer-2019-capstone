1120ebc7f88bc7aa02e0442707cab6a3ee699af4
Karanveer Singh <karansingh98123@gmail.com>
2019-06-17 12:10:05 -0400

    combined login and signup reducers in the store

--- capstone-2019/src/Store.jsx
+++ capstone-2019/src/Store.jsx
@@ -1,5 +1,8 @@
 import {createStore, applyMiddlware, combineReducers} from 'react';
 import thunkMiddleware from 'react-thunk';
 import loginReducer from './reducers/LoginReducer';
+import signUpReducer from './reducer/SignUpReducer'
+
+const reducers = combineReducers(loginReducer)
 
 export default createStore(loginReducer, applyMiddlware(thunkMiddleware));
