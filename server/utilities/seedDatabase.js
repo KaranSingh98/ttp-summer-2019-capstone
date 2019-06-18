fa36505ebe11caae8c5df2025f970b0ef91e9b21
Jasper Cheung <wymanjasper@gmail.com>
2019-06-18 16:43:02 -0400

    favorites routes done

--- server/utilities/seedDatabase.js
+++ server/utilities/seedDatabase.js
@@ -4,19 +4,27 @@ const users  = require('../data/users');
 
 
 const populateUserTable = async (users) => {
+  let lebronJames = await Player.create(players[0]);
+  let ikeAnigbogu = await Player.create(players[1]);
+
   for (let i = 0; i < users.length; i++) {
     let currentUser = users[i];
     let builtUser = await User.build(currentUser);
     await builtUser.save();
+    await builtUser.addPlayer(lebronJames);
+    await builtUser.addPlayer(ikeAnigbogu);
+
   }
 };
-const populatePlayerTable = async (players) => {
+/*
+  const populatePlayerTable = async (players) => {
   for (let i = 0; i < players.length; i++) {
-    let currentPlayer = players[i];
-    let builtPlayer = await Player.build(currentPlayer);
-    await builtPlayer.save();
+  let currentPlayer = players[i];
+  let builtPlayer = await Player.build(currentPlayer);
+  await builtPlayer.save();
   }
-};
+  };
+*/
 
 
 
@@ -25,7 +33,7 @@ const seedDatabase = async () => {
   try {
 
     await populateUserTable(users);
-    await populatePlayerTable(players);
+    // await populatePlayerTable(players);
 
     console.log("Successfully seeded!");
     process.exit(0);
