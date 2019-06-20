b6eacd358da8483f02cb3c0dc1acd49c27ee10de
Karanveer Singh <karansingh98123@gmail.com>
2019-06-19 20:24:04 -0400

    got favorite players to display on feeds correctly along with nicer game date formatting

--- capstone-2019/src/components/Feed.jsx
+++ capstone-2019/src/components/Feed.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {connect} from 'react-redux';
+import {teamObject} from './teams';
 
 
 const mapStates = (state) => {
@@ -10,6 +11,7 @@ const mapStates = (state) => {
         //favorites: state.favoritesReducer.favorites,
         user: state.loginReducer.user
     };
+
 }; // end of mapStates
 
 
@@ -34,32 +36,40 @@ class Feed extends Component {
     }; // end of componentDidMount
 
 
+    // fecthes the stats of the players favorited by the user
     getPlayerInfo = () => {
 
         const favorites = this.state.favorites;
 
+        // have to go through each favorites player ID individually due to API constraints
         for(let i = 0; i < favorites.length; i++) {
 
             let curr = favorites[i];
 
+            // stats fetched are for the current (2018-19) season only
             axios.get(`https://www.balldontlie.io/api/v1/stats?seasons[]=2018&player_ids[]=${curr}&per_page=100`)
                 .then(res => {
 
-                    const players = res.data.data.sort((a, b) => {
+                    // stats are sorted by date because the stats come in with
+                    // the games out of order
+                    const stats = res.data.data.sort((a, b) => {
 
                         return new Date(b.game.date) - new Date(a.game.date);
                     });
 
-                    let playerName = players[0].player.first_name + " " + players[0].player.last_name;
+                    let playerName = stats[0].player.first_name + " " + stats[0].player.last_name;
 
+                    // an object is created to give each player their games stats
+                    // so that when these stats are outputted, the name of the player
+                    // is not displayed each time the stats are mapped
                     const newPlayer = {
                         playerName: playerName,
-                        games: players
+                        stats: stats
                     };
 
                     this.setState({
                         playersInfo: this.state.playersInfo.concat(newPlayer)
-                    })
+                    });
 
                 })
                 .catch(err => console.log(err));
@@ -68,9 +78,31 @@ class Feed extends Component {
     } // end of getPlayerInfo
 
 
-    render() {
+    // returns the opposing team faced by the player in a particular game
+    getTeam = (playerTeamId, homeTeamId, awayTeamId) => {
 
-        console.log(this.state.playersInfo)
+        const teams = teamObject.data;
+
+        for(let i = 0; i < teams.length; i++) {
+            if(playerTeamId == homeTeamId && teams[i].id == awayTeamId)
+                return teams[i].full_name;
+            else if(playerTeamId == awayTeamId && teams[i].id == homeTeamId)
+                return teams[i].full_name;
+        }
+
+    } // end of getTeam
+
+
+    // returns a nicer looking date, as opposed to the one from the API
+    getDate = (date) => {
+        const newDate = new Date(date);
+
+        return newDate.toDateString();
+
+    } // end of getDate
+
+
+    render() {
 
         return (
 
@@ -78,21 +110,34 @@ class Feed extends Component {
 
                 <h1> Feed </h1>
 
+                {/* if the user is logged in or not */}
                 {this.props.user.id ? (
 
                     <div>
 
                         {this.state.playersInfo.map(player =>
 
-                            <div key={player.games[0].player.id}>
-                                <h1> {player.playerName} </h1>
-                                {player.games.map(game =>
+                            <div key={player.stats[0].player.id}>
 
-                                    <div key={game.game.id}>
-                                        <h2> Against: </h2>
-                                        <h4> Points Scored: {game.pts} </h4>
+                                <h3> {player.playerName} </h3>
+
+                                {/* map only the 5 most recent games */}
+                                {player.stats.slice(0, 5).map(stat =>
+
+                                    <div key={stat.game.id}>
+
+                                        <h4>
+                                            Against: {this.getTeam(stat.team.id,
+                                            stat.game.home_team_id, stat.game.visitor_team_id)} on
+                                            {" "} {this.getDate(stat.game.date)}
+                                        </h4>
+
+                                        <p> Points Scored: {stat.pts} </p>
+
                                     </div>
                                 )}
+                                <hr/>
+
                             </div>
                         )}
 
