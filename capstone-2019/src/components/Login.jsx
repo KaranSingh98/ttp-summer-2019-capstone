3238cd9142650d39e9b5c8f615b74ac2fcbde47e
Karanveer Singh <karansingh98123@gmail.com>
2019-06-20 10:30:56 -0400

    removed Link import from Login.jsx

--- capstone-2019/src/components/Login.jsx
+++ capstone-2019/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import NavBar from './NavBar';
 import {userLogin} from '../actions/LoginActions';
 
